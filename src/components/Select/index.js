3aac1688d67f6f2c30cd57c9f8a55bccec75f030
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2018-09-16 00:06:29 -0300

    feat: add disabled select behavior

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 const Select = () => (
   <div className="select">
-    <select>
+    <select disabled>
       <option value="">-</option>
       <option value="">1</option>
       <option value="">2</option>
