dfb8b55e98ed95617bc2fe1734ce42af7097b043
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2018-09-15 22:56:19 -0300

    feat: add static card strucure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Card from './components/Card';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        olá mundo!
+        <Card />
       </div>
     );
   }
---
+++ src/components/Card/index.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card">
+        <div className="detail">
+          <div className="block">
+            <p>Royal - lote 1</p>
+            <p className="price">
+              <span>R$ 1.000,00</span>
+              <span>(+ R$ 89,90 em taxas)</span>
+            </p>
+          </div>
+          <div className="select">
+            <select>
+              <option value="">-</option>
+              <option value="">1</option>
+              <option value="">2</option>
+              <option value="">3</option>
+            </select>
+          </div>
+        </div>
+        <div className="description">
+          Mostrar Descrição
+        </div>
+      </div> 
+    );
+  }
+};
+
+export default Card;
+
