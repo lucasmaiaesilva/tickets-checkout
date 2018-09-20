19379e2fb4d636d41a41513b0b175f9347e07f4a
lucasmaiaesilva <lucasmaiaesilva@gmail.com>
2018-09-20 18:12:37 -0300

    feat: add tests for the select component

---
+++ src/components/Select/index.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow, mount } from 'enzyme';
+import Select from './';
+
+describe('[Component] Button', () => {
+  const props = {
+    quantities: [2, 3, 4, 5],
+    ticketId: 'asd',
+    handleChange: jest.fn(),
+    price: 400,
+    discount: 2,
+  };
+  it('render correctly', () => {
+    const wrapper = shallow(<Select {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('render correctly when disabled', () => {
+    const wrapper = shallow(<Select {...props} disabled />);
+    expect(wrapper).toMatchSnapshot();
+  });
+});
+
